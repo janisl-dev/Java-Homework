ac2d65258de7fe6e69440c7a4d84ab168519e4b1
Janis Locmelis <locmelis.j@gmail.com>
2021-02-21 17:30:42 +0200

    First homework added

--- src/com/rcs/Homework1/Main.java
+++ src/com/rcs/Homework1/Main.java
@@ -1,8 +1,15 @@
 package com.rcs.Homework1;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    // write your code here
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ievadi grādus pēc Fārenheita: ");
+        float F = scanner.nextFloat();
+        float C = ((5*(F-32))/9);
+        System.out.println("Grādi pēc Fārenheita: " + F);
+        System.out.println("Grādi pēc Celsija: " + C);
     }
 }
