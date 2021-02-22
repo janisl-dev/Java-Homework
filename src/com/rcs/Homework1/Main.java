51a831ed2367335162b809ac83fa31d75f848644
Janis Locmelis <locmelis.j@gmail.com>
2021-02-22 22:57:50 +0200

    First homework updated

--- src/com/rcs/Homework1/Main.java
+++ src/com/rcs/Homework1/Main.java
@@ -1,15 +1,20 @@
 package com.rcs.Homework1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        DecimalFormat df = new DecimalFormat("#.##");
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ievadi grādus pēc Fārenheita: ");
         float F = scanner.nextFloat();
-        float C = ((5*(F-32))/9);
-        System.out.println("Grādi pēc Fārenheita: " + F);
-        System.out.println("Grādi pēc Celsija: " + C);
+        scanner.close();
+        float C = 5 * (F - 32) / 9;
+        char c = '\u2103';
+        char f = '\u2109';
+        System.out.println("Grādi pēc Fārenheita: " + df.format(F) + f);
+        System.out.println("Grādi pēc Celsija: " + df.format(C) + c);
     }
 }
