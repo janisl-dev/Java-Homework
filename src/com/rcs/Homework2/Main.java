bf004e16c9c6721e7b64964de6e89b9148e181d2
Janis Locmelis <locmelis.j@gmail.com>
2021-02-28 11:24:40 +0200

    Second homework updated

--- src/com/rcs/Homework2/Main.java
+++ src/com/rcs/Homework2/Main.java
@@ -6,17 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ievadi X koordinātu ");
+        System.out.print("Ievadi X koordinātu: ");
         int x = scanner.nextInt();
-        System.out.print("Ievadi Y koordinātu ");
+        System.out.print("Ievadi Y koordinātu: ");
         int y = scanner.nextInt();
         scanner.close();
-        if ((x > -2 && x < 3) && (y > -4 && y < 1)) {
-            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas figūras iekšpusē");
-        } else if ((x == -2 || x == 3) && (y == 1 || y == -4)) {
-            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas uz figūras līnijas");
+        if (x > -2 && x < 3 && y > -4 && y < 1) {
+            System.out.println("Koordinātas (" + x + ";" + y + ") atrodas figūras iekšpusē");
+        } else if (x >= -2 && x <= 3 && y >= -4 && y <= 1) {
+            System.out.println("Koordinātas (" + x + ";" + y + ") atrodas uz figūras līnijas");
         } else {
-            System.out.println("Koordinātas (" + x + "; " + y + ") atrodas figūras ārpusē");
+            System.out.println("Koordinātas (" + x + ";" + y + ") atrodas figūras ārpusē");
 
         }
 
